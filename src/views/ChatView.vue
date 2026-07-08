76ecbe7fa1843d7cda5819ffad4357311fe28eb8
liruiqing <liruiqing@mediway.cn>
2026-07-08 08:58:49 +0800

    Update ChatView.vue to disable RAG functionality by default

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -5,7 +5,7 @@ import { useChatStore } from '@/stores/chat'
 
 const chatStore = useChatStore()
 const input = ref('')
-const useRag = ref(true)
+const useRag = ref(false)
 const loading = ref(false)
 const error = ref('')
 const listRef = ref<HTMLElement | null>(null)
